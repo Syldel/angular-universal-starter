ade8548108f783c9040208e11d9c13f5c2ef865a
Syldel <syldeles@hotmail.fr>
2019-02-21 21:59:29 +0100

    first commit with files generated with @angular/cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'beerme-web-app'`, () => {
+  it(`should have as title 'angular-web-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('beerme-web-app');
+    expect(app.title).toEqual('angular-web-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beerme-web-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-web-app!');
   });
 });
