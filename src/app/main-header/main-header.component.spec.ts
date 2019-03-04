bfcae6e321bfa2af70ccaf579735b96fcf93db7e
Syldel <syldeles@hotmail.fr>
2019-03-04 23:33:26 +0100

    feat (style) : Use "bootstrap-sass"

--- src/app/main-header/main-header.component.spec.ts
+++ src/app/main-header/main-header.component.spec.ts
@@ -2,12 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainHeaderComponent } from './main-header.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('MainHeaderComponent', () => {
   let component: MainHeaderComponent;
   let fixture: ComponentFixture<MainHeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ MainHeaderComponent ]
     })
     .compileComponents();
