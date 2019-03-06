b4b273f0f5678e38cb4bb2fe23bf50c04534ff91
Syldel <syldeles@hotmail.fr>
2019-03-06 14:51:41 +0100

    feat (e2e ) : Manage welcome message

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-web-app!');
+    expect(page.getTitleText()).toEqual('Universal JavaScript angular-web-app!');
   });
 
   afterEach(async () => {
