14b8f61ffcc356392cb693b16e8aafb6806aefb2
Syldel <syldeles@hotmail.fr>
2019-03-26 22:30:37 +0100

    chore (package) : Upgrade "amazon-s3-cdn" to "0.2.1"

--- s3-config.js
+++ s3-config.js
@@ -7,7 +7,6 @@ config.fileExtensions = [
     'woff',
     'svg',
     'ttf',
-    'woff',
     'eot',
     'ico'
 ];
@@ -21,5 +20,6 @@ config.s3Options = {
 config.localDir = './dist/browser';
 config.s3Bucket = process.env.AWS_S3_BUCKET;
 config.subDir = process.env.NODE_ENV || 'development';
+config.cdnDomain = process.env.CDN_DOMAIN;
 
 module.exports = config;
